f406b41e85bd37ea342ae5a06a3cd317e7d492ab
imenovass <imenovaaq@gmail.com>
2025-06-05 17:47:36 +0600

    feat: login

--- src/pages/LoginPage/ui/LoginPage.jsx
+++ src/pages/LoginPage/ui/LoginPage.jsx
@@ -5,6 +5,7 @@ import { Button, Form, Input } from 'antd'
 import { loginFx } from '../../../features/auth/model/pbAuth'
 import { setSession } from '../../../features/auth/model/session'
 import Logo from '../../../shared/ui/Logo/Logo'
+import img from "../../../assets/img/login.jpg"
 
 import './styles.scss'
 
@@ -25,41 +26,45 @@ export const LoginPage = () => {
   }
 
   return (
-    <div className="auth-page">
-      <div className="auth-left">
-        <div className="content">
-        <div className="logo-wrapper">
-          <Logo />
+    <div className="login-wrapper">
+      <div className="login-card">
+        <div className="login-left">
+          <div className="logo-wrapper">
+            <Logo />
+          </div>
+
+
+          <h2 className="auth-subtitle">Добро пожаловать</h2>
+          <h1 className=" auth-title">Войти в аккаунт</h1>
+
+          <Form layout="vertical" onFinish={onFinish} className="auth-form">
+            {error && <p className="error-text">{error}</p>}
+
+            <Form.Item name="login" rules={[{ required: true, message: 'Введите логин' }]}>
+              <Input placeholder="Логин" size="large" />
+            </Form.Item>
+
+            <Form.Item name="password" rules={[{ required: true, message: 'Введите пароль' }]}>
+              <Input.Password placeholder="Пароль" size="large" />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" block size="large">
+                Далее
+              </Button>
+            </Form.Item>
+          </Form>
+
+          <p className="footer-text">
+            Если вы не помните логин или пароль, обратитесь к администратору.
+          </p>
         </div>
 
-        <h2 className="auth-title">Start your journey</h2>
-        <h1 className="auth-subtitle">Sign In to Codify</h1>
+        <div className="login-right">
+          <img src={img} alt="Login Visual" />
+        </div>
 
-        <Form layout="vertical" onFinish={onFinish} className="auth-form">
-          {error && <p className="error-text">{error}</p>}
-
-          <Form.Item name="login" rules={[{ required: true, message: 'Введите логин' }]}>
-            <Input placeholder="Логин" size="large" />
-          </Form.Item>
-
-          <Form.Item name="password" rules={[{ required: true, message: 'Введите пароль' }]}>
-            <Input.Password placeholder="Пароль" size="large" />
-          </Form.Item>
-
-          <Form.Item>
-            <Button type="primary" htmlType="submit" block size="large">
-              Далее
-            </Button>
-          </Form.Item>
-        </Form>
-
-        <p className="footer-text">
-          Если вы не помните логин или пароль, обратитесь к администратору.
-        </p>
       </div>
-      </div>
-
-      <div className="auth-right" />
     </div>
   )
 }
